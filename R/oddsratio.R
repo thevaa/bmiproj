6b8b867ae364254d1a8df826c67b956810317f9c
Karl Broman <kbroman@gmail.com>
2016-05-06 15:50:07 -0500

    Fix oddsratio documentation

--- R/oddsratio.R
+++ R/oddsratio.R
@@ -1,16 +1,15 @@
 #' Odds ratio for 2x2 table with Confidence Interval
 #'
-#'  This functions prints a list of odds ratio and confidence interval for a 2x2
-#'  count data
+#' This functions prints a list of odds ratio and confidence interval for a 2x2
+#' count data
 #'
-#'   @param x a vector of length 4 with the data in the order of a, b, c, d
-#'   @param CI a numeric vector indicating the two sided confidence interval
-#'   @return a list of two elements: element 1 is the odds ratio, element 2 is a vector of length
-#'   two of lower bound and upper bounds of the odds ratio
-#'   @author Thevaa Chandereng
-#'   @seealso \code{mean}
-#'   @export
-#'
+#' @param x a vector of length 4 with the data in the order of a, b, c, d
+#' @param CI a numeric vector indicating the two sided confidence interval
+#' @return a list of two elements: element 1 is the odds ratio, element 2 is a vector of length
+#' two of lower bound and upper bounds of the odds ratio
+#' @author Thevaa Chandereng
+#' @seealso \code{mean}
+#' @export
 oddsratio <- function(x, CI = 0.95){
   stopifnot(all(sapply(x, is.numeric)))
   stopifnot(length(x) == 4)
