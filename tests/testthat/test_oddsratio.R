b6c33f7f47723089ab0791aaf8f90011dfb8f9c6
Karl Broman <kbroman@gmail.com>
2016-05-06 15:47:45 -0500

    Small changes in oddsratio test
      - odds.ratio -> oddsratio
      - 3rd test: more precise expected result; testing just confidence
        interval for that

--- tests/testthat/test_oddsratio.R
+++ tests/testthat/test_oddsratio.R
@@ -1,7 +1,7 @@
 context("odds ratio")
 test_that("the 2x2 odds ratio is", {
-  expect_equal(odds.ratio(c(5,5,5,5))$odds, 1)
-  expect_equal(odds.ratio(c(10,5,5,10))$odds, 4)
-  expect_equal(odds.ratio(c(400, 400, 400, 400), 0.90), c(0.8483302, 1.1787863))
-  expect_error(odds.ratio(c(50, NA, "c", 3)))
+  expect_equal(oddsratio(c(5,5,5,5))$odds, 1)
+  expect_equal(oddsratio(c(10,5,5,10))$odds, 4)
+  expect_equal(oddsratio(c(400, 400, 400, 400), 0.90)$conf.int, c(0.8483301741208367, 1.1787863151706770))
+  expect_error(oddsratio(c(50, NA, "c", 3)))
 })
